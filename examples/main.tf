73e7ade1d2269f88c03636948703e21d7f9187c1
Anton Patsev <patsev.anton@gmail.com>
2024-07-28 19:14:26 +0600

    Исправил некоторые ошибки tflint

--- examples/main.tf
+++ examples/main.tf
@@ -1,18 +1,18 @@
 module "mongodb_cluster" {
   source = "../"
 
-  network_id   = "xxxx"
-  subnet_id    = "xxxx"
+  network_id = "xxxx"
+  subnet_id  = "xxxx"
 
-  cluster_name           = "test-cluster"
-  environment            = "PRESTABLE"
-  mongodb_version        = "5.0"
-  labels                 = { test_key = "test_value" }
-  database_name          = "testdb"
-  user_name              = "john"
-  user_password          = "password"
-  resources_mongod_preset = "s2.small"
+  cluster_name               = "test-cluster"
+  environment                = "PRESTABLE"
+  mongodb_version            = "5.0"
+  labels                     = { test_key = "test_value" }
+  database_name              = "testdb"
+  user_name                  = "john"
+  user_password              = "password"
+  resources_mongod_preset    = "s2.small"
   resources_mongod_disk_size = 16
   resources_mongod_disk_type = "network-hdd"
-#   maintenance_window_type = "ANYTIME"
+  #   maintenance_window_type = "ANYTIME"
 }
